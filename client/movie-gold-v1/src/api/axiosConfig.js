3a5688e95ae5d09a39b0c7e9c415f5d6b1133297
Hristova, Maria (Student) <m.hristova@lancaster.ac.uk>
2023-03-12 00:36:29 +0000

    issue with connecting to axios api....

---
+++ client/movie-gold-v1/src/api/axiosConfig.js
@@ -0,0 +1,11 @@
+import axios from 'axios';
+
+export default axios.create({
+    baseURL: 'https://9c96-103-106-239-104.ap.ngrok.io',
+    headers: {
+        "ngrok-skip-browser-warning": "true",
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Headers': '*',
+        'Access-Control-Allow-Credentials': 'true'
+    }
+});
